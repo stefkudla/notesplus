8125824d57c4503ab76046bd773fa55680eba02c
stefkudla <stefankudla@gmail.com>
2023-03-10 17:42:50 -0500

    replace img with Next Image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 
 export const Header = () => {
   const { data: sessionData } = useSession();
@@ -16,10 +17,12 @@ export const Header = () => {
               className="btn-ghost btn-circle avatar btn"
               onClick={() => void signOut()}
             >
-              <div className="w-10 rounded-full">
-                <img
+              <div className="h-10 w-10 rounded-full bg-secondary">
+                <Image
                   src={sessionData?.user?.image ?? ""}
                   alt={sessionData?.user?.name ?? ""}
+                  width={40}
+                  height={40}
                 />
               </div>
             </label>
